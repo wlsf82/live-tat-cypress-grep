823cfe43781d339d9f42304890eda093fe6471cc
Walmyr <wlsf82@gmail.com>
2021-05-18 02:15:37 +0200

    Remove code duplication with custom command

--- cypress/integration/editDestination.spec.js
+++ cypress/integration/editDestination.spec.js
@@ -7,36 +7,26 @@ describe('Edit destinations page', () => {
   })
 
   it("doesn't update if minimum name and description aren't met", { tags: ['@edit-destination', '@critical']}, () => {
-    cy.get('form #destination_name')
-      .should('be.visible')
-      .clear()
-      .type('ab')
-    cy.get('form #destination_description')
-      .should('be.visible')
-      .clear()
-      .type('Abcdefghi')
+    const destination = {
+      name: 'Ab',
+      description: 'Abcdefghi'
+    }
 
-    cy.get('input[type="submit"]').click()
+    cy.fillFormAndSubmit(destination)
 
     cy.get('.field_with_errors input').should('have.length', 2)
   })
 
   it('successfully updates destination name and description', { tags: ['@edit-destination', '@smoke', '@critical']}, () => {
-    const newName = `Name ${Date.now()}`
-    const newDesc = `Description ${Date.now()}`
-    cy.get('form #destination_name')
-      .should('be.visible')
-      .clear()
-      .type(newName)
-    cy.get('form #destination_description')
-      .should('be.visible')
-      .clear()
-      .type(newDesc)
+    const destinationUpdated = {
+      name: `Name ${Date.now()}`,
+      description: `Description ${Date.now()}`
+    }
 
-    cy.get('input[type="submit"]').click()
+    cy.fillFormAndSubmit(destinationUpdated)
 
     cy.url().should('be.equal', `${Cypress.config('baseUrl')}/destinations/${destination}`)
-    cy.get('h2').should('have.text', newName)
-    cy.get('p').should('have.text', newDesc)
+    cy.get('h2').should('have.text', destinationUpdated.name)
+    cy.get('p').should('have.text', destinationUpdated.description)
   })
 })
